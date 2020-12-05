2665502fd5ca5533773a18fe7b5c7131c59967ce
defhax <alexjtavarez@gmail.com>
2020-12-04 20:42:48 -0500

    Day 3 (not yet complete).

---
+++ aoc_2020/day_3.py
@@ -0,0 +1,32 @@
+with open('day_3-input.txt') as garden:
+  garden = list(garden)
+  newgarden = []
+  for row in garden:
+    if isinstance(row,str):
+      row = row.strip()
+    newgarden.append(list(row))
+  garden = newgarden
+
+del garden[0]
+
+unitmove = 3
+count = 0
+# rowcount = 0
+
+for row in garden:
+  if unitmove < len(row):
+    if row[unitmove] == "#":
+      row[unitmove] = "X"
+      count += 1
+    elif row[unitmove] == ".":
+      row[unitmove] = "0"
+    unitmove += 3
+  # elif unitmove >= len(row):
+  #   unitmove = 0
+  #   if row[unitmove] == "#":
+  #     row[unitmove] = "X"
+  #   elif row[unitmove] == ".":
+  #     row[unitmove] = "0"
+  print(row)
+
+print(count)
